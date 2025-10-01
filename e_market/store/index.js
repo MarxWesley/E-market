cab01db612980aa69a20582296567c8d341705c1
Wesley Marques <Marques_bertin@hotmail.com>
2025-10-01 00:38:00 -0300

    fix: arquivos removidos

--- e_market/store/index.js
+++ e_market/store/index.js
@@ -1,11 +1,9 @@
 // store/index.js
 import { configureStore } from "@reduxjs/toolkit";
-import { tasksReducer } from "./features/taskSlice";
 import authReducer from "./features/authSlice"; // ⬅ default
 
 export const store = configureStore({
   reducer: {
-    tasks: tasksReducer,
     auth: authReducer, // ⬅ registra o auth
   },
 });
