f915d141bc1141370f2900a68031b36f2dcb5cb9
Pedro Vinícius <pedromichelwork@gmail.com>
2025-10-01 00:16:57 -0300

    everything working

--- e_market/screens/ItemScreen.jsx
+++ e_market/screens/ItemScreen.jsx
@@ -8,31 +8,61 @@ import {
   TouchableOpacity,
   ScrollView,
   KeyboardAvoidingView,
-  Platform
+  Platform,
+  Alert,
 } from "react-native";
 import * as ImagePicker from "expo-image-picker";
 import { Picker } from "@react-native-picker/picker";
 import { v4 as uuidv4 } from "uuid";
 import { useForm, Controller } from "react-hook-form";
-import * as Icon from "react-native-vector-icons"
+import { ImagePlus } from "lucide-react-native";
+import DropDownPicker from "react-native-dropdown-picker";
+
 
 export default function ItemScreen() {
   const [images, setImages] = useState([]);
   const [focusedField, setFocusedField] = useState(null); // controla qual campo está focado
+  const [categoryOpen, setCategoryOpen] = useState(false);
+  const [conditionOpen, setConditionOpen] = useState(false);
 
-  const { control, handleSubmit, formState: { errors } } = useForm({
+  const [category, setCategory] = useState([
+    { label: "Roupas", value: "Roupas" },
+    { label: "Eletrônicos", value: "Eletrônicos" },
+    { label: "Móveis", value: "Móveis" },
+    { label: "Entretenimento", value: "Entretenimento" },
+    { label: "Esporte", value: "Esporte" },
+    { label: "Casa e jardim", value: "Casa e jardim" },
+    { label: "Outro", value: "Outro" },
+  ]);
+
+  const [condition, setCondition] = useState([
+    { label: "Novo", value: "Novo" },
+    { label: "Usado - estado de novo", value: "Usado - estado de novo" },
+    { label: "Usado - em boas condições", value: "Usado - em boas condições" },
+    { label: "Usado - em condições razoáveis", value: "Usado - em condições razoáveis" },
+  ]);
+
+  const {
+    control,
+    handleSubmit,
+    formState: { errors },
+    reset,
+  } = useForm({
     defaultValues: {
       title: "",
       price: "",
       category: "",
       condition: "",
       description: "",
-    }
+    },
   });
 
   const pickImage = async () => {
     let result = await ImagePicker.launchImageLibraryAsync({
-      mediaTypes: ImagePicker.MediaTypeOptions.Images,
+      mediaTypes: [
+        ImagePicker.MediaTypeOptions.Images,
+        ImagePicker.MediaTypeOptions.Videos,
+      ],
       allowsEditing: true,
       quality: 1,
       selectionLimit: 0,
@@ -52,23 +82,24 @@ export default function ItemScreen() {
       images,
     };
 
-    // try {
-    //   const response = await fetch("http://localhost:3000/items", {
-    //     method: "POST",
-    //     headers: { "Content-Type": "application/json" },
-    //     body: JSON.stringify(newItem),
-    //   });
+    try {
+      const response = await fetch("http://localhost:3000/product", {
+        method: "POST",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify(newItem),
+      });
 
-    //   if (response.ok) {
-    //     Alert.alert("Sucesso", "Item publicado!");
-    //     navigation.goBack();
-    //   } else {
-    //     Alert.alert("Erro", "Não foi possível publicar.");
-    //   }
-    // } catch (error) {
-    //   console.error(error);
-    //   Alert.alert("Erro", "Verifique o servidor.");
-    // }
+      if (response.ok) {
+        Alert.alert("Sucesso", "Item publicado!");
+        reset();
+        setImages([]);
+      } else {
+        Alert.alert("Erro", "Não foi possível publicar.");
+      }
+    } catch (error) {
+      console.error(error);
+      Alert.alert("Erro", "Verifique o servidor.");
+    }
 
     console.log(newItem);
     alert("Item criado! Confira o console.");
@@ -85,12 +116,13 @@ export default function ItemScreen() {
             <Image key={index} source={{ uri }} style={styles.image} />
           ))}
           <TouchableOpacity style={styles.addImageButton} onPress={pickImage}>
-            <Text style={styles.addPhotoText}>+</Text>
-            <Text>
+            <Text style={styles.addPhotoText}>
+              <ImagePlus />
+            </Text>
+            <Text style={{ fontSize: 13, color: "#333", textAlign: "center" }}>
               Adicionar foto
             </Text>
           </TouchableOpacity>
-
         </View>
         {/* Título */}
         <Controller
@@ -100,70 +132,109 @@ export default function ItemScreen() {
           render={({ field: { onChange, value } }) => (
             <TextInput
               placeholder="Título"
-              style={styles.input}
+              style={[
+                styles.input,
+                focusedField === "title" && { borderColor: "#319BE5" },
+              ]}
               value={value}
               onChangeText={onChange}
+              onFocus={() => setFocusedField("title")}
+              onBlur={() => setFocusedField(null)}
             />
           )}
         />
-        {errors.title && <Text style={styles.errorText}>{errors.title.message}</Text>}
+        {errors.title && (
+          <Text style={styles.errorText}>{errors.title.message}</Text>
+        )}
         {/* Preço */}
         <Controller
           control={control}
           name="price"
           rules={{
             required: "O preço é obrigatório",
-            pattern: { value: /^\d+(\.\d{1,2})?$/, message: "Digite um preço válido" }
+            pattern: {
+              value: /^\d+(\.\d{1,2})?$/,
+              message: "Digite um preço válido",
+            },
           }}
           render={({ field: { onChange, value } }) => (
             <TextInput
               placeholder="Preço"
-              style={styles.input}
+              style={[
+                styles.input,
+                focusedField === "price" && { borderColor: "#319BE5" },
+              ]}
               value={value}
               onChangeText={onChange}
+              onFocus={() => setFocusedField("price")}
+              onBlur={() => setFocusedField(null)}
               keyboardType="numeric"
             />
           )}
         />
-        {errors.price && <Text style={styles.errorText}>{errors.price.message}</Text>}
+        {errors.price && (
+          <Text style={styles.errorText}>{errors.price.message}</Text>
+        )}
         {/* Categoria */}
         <Controller
           control={control}
           name="category"
           rules={{ required: "A categoria é obrigatória" }}
           render={({ field: { onChange, value } }) => (
-            <View style={styles.pickerContainer}>
-              <Picker selectedValue={value} onValueChange={onChange}>
-                <Picker.Item label="Selecione uma categoria" value="" />
-                <Picker.Item label="Roupas" value="Roupas" />
-                <Picker.Item label="Eletrônicos" value="Eletrônicos" />
-                <Picker.Item label="Móveis" value="Móveis" />
-                <Picker.Item label="Entretenimento" value="Entretenimento" />
-                <Picker.Item label="Esporte" value="Esporte" />
-                <Picker.Item label="Casa e jardim" value="Casa e jardim" />
-              </Picker>
+            <View>
+              <DropDownPicker
+                style={styles.pickerContainer}
+                open={categoryOpen}
+                value={value}
+                items={category}
+                setOpen={setCategoryOpen}
+                setValue={onChange}
+                setItems={setCategory}
+                placeholder="Selecione a categoria"
+                listMode="MODAL" // não abre modal, abre inline
+                modalProps={{
+                  animationType: "slide",
+                }}
+                modalAnimationType="slide" // Tipo de animação do modal
+                zIndex={2000}
+                zIndexInverse={2000}
+              />
             </View>
           )}
         />
-        {errors.category && <Text style={styles.errorText}>{errors.category.message}</Text>}
+        {errors.category && (
+          <Text style={styles.errorText}>{errors.category.message}</Text>
+        )}
         {/* Condição */}
         <Controller
           control={control}
           name="condition"
           rules={{ required: "A condição é obrigatória" }}
           render={({ field: { onChange, value } }) => (
-            <View style={styles.pickerContainer}>
-              <Picker selectedValue={value} onValueChange={onChange}>
-                <Picker.Item label="Selecione a condição" value="" />
-                <Picker.Item label="Novo" value="Novo" />
-                <Picker.Item label="Usado - estado de novo" value="Usado - estado de novo" />
-                <Picker.Item label="Usado - em boas condições" value="Usado - em boas condições" />
-                <Picker.Item label="Usado - em condições razoáveis" value="Usado - em condições razoáveis" />
-              </Picker>
+            <View>
+              <DropDownPicker
+                style={styles.pickerContainer}
+                open={conditionOpen}
+                value={value}
+                items={condition}
+                setOpen={setConditionOpen}
+                setValue={onChange}
+                setItems={setCondition}
+                placeholder="Selecione a condição"
+                listMode="MODAL" // não abre modal, abre inline
+                modalProps={{
+                  animationType: "slide",
+                }}
+                modalAnimationType="slide" // Tipo de animação do modal
+                zIndex={2000}
+                zIndexInverse={2000}
+              />
             </View>
           )}
         />
-        {errors.condition && <Text style={styles.errorText}>{errors.condition.message}</Text>}
+        {errors.condition && (
+          <Text style={styles.errorText}>{errors.condition.message}</Text>
+        )}
         {/* Descrição */}
         <Controller
           control={control}
@@ -171,16 +242,28 @@ export default function ItemScreen() {
           render={({ field: { onChange, value } }) => (
             <TextInput
               placeholder="Descrição (opcional)"
-              style={[styles.input, { height: 100, marginBottom: 170 }]}
+              style={[
+                styles.input,
+                { height: 100, textAlignVertical: "top" },
+                focusedField === "description" && { borderColor: "#319BE5" },
+              ]}
               value={value}
               onChangeText={onChange}
+              onFocus={() => setFocusedField("description")}
+              onBlur={() => setFocusedField(null)}
               multiline
             />
           )}
         />
-        <TouchableOpacity style={styles.publishButton} onPress={handleSubmit(onSubmit)}>
-          <Text style={styles.publishButtonText}>Publicar</Text>
-        </TouchableOpacity>
+        {/* Botão Publicar */}
+        <View style={{ marginBottom: 30, justifyContent: "center" }}>
+          <TouchableOpacity
+            style={styles.publishButton}
+            onPress={handleSubmit(onSubmit)}
+          >
+            <Text style={styles.publishButtonText}>Publicar</Text>
+          </TouchableOpacity>
+        </View>
       </ScrollView>
     </KeyboardAvoidingView>
   );
@@ -190,21 +273,21 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     padding: 20,
-    backgroundColor: "#fff"
+    backgroundColor: "#fff",
   },
   imagesContainer: {
     flexDirection: "row",
     alignItems: "center",
     justifyContent: "center",
     marginBottom: 20,
-    flexWrap: "wrap"
+    flexWrap: "wrap",
   },
   image: {
     width: 80,
     height: 80,
     borderRadius: 10,
     marginRight: 10,
-    marginBottom: 10
+    marginBottom: 10,
   },
   addImageButton: {
     width: 80,
@@ -212,18 +295,19 @@ const styles = StyleSheet.create({
     borderRadius: 10,
     backgroundColor: "#f0f0f0",
     justifyContent: "center",
-    alignItems: "center", marginBottom: 10
+    alignItems: "center",
+    marginBottom: 10,
   },
   addPhotoText: {
     fontSize: 30,
-    color: "#333"
+    color: "#333",
   },
   input: {
     borderWidth: 1,
     borderColor: "#ccc",
     borderRadius: 8,
     padding: 10,
-    marginBottom: 15
+    marginBottom: 15,
   },
   pickerContainer: {
     borderWidth: 1,
@@ -233,22 +317,22 @@ const styles = StyleSheet.create({
     height: 50,
     justifyContent: "center",
     backgroundColor: "#fff",
-    overflow: "hidden"
+    overflow: "hidden",
   },
   publishButton: {
     backgroundColor: "#319BE5",
     padding: 15,
     borderRadius: 8,
     lignItems: "center",
-    marginTop: 10
+    marginTop: 10,
   },
   publishButtonText: {
     color: "#fff",
     fontSize: 16,
-    fontWeight: "bold"
+    fontWeight: "bold",
   },
   errorText: {
     color: "red",
-    marginBottom: 10
+    marginBottom: 10,
   },
 });
--- e_market/screens/VehicleScreen.jsx
+++ e_market/screens/VehicleScreen.jsx
@@ -1,3 +1,425 @@
+import React, { useState } from "react";
+import {
+  View,
+  Text,
+  TextInput,
+  StyleSheet,
+  Image,
+  TouchableOpacity,
+  ScrollView,
+  KeyboardAvoidingView,
+  Platform,
+  Alert,
+} from "react-native";
+import * as ImagePicker from "expo-image-picker";
+import { Picker } from "@react-native-picker/picker";
+import { v4 as uuidv4 } from "uuid";
+import { useForm, Controller, set } from "react-hook-form";
+import { ImagePlus } from "lucide-react-native";
+import DropDownPicker from "react-native-dropdown-picker";
+
 export default function VehicleScren(params) {
-    
+  const [images, setImages] = useState([]);
+  const [focusedField, setFocusedField] = useState(null); // controla qual campo está focado
+
+  const [typeOpen, setTypeOpen] = useState(false);
+  const [yearOpen, setYearOpen] = useState(false);
+
+  const [typeItems, setTypeItems] = useState([
+    { label: "Carros e caminhões", value: "Carros e caminhões" },
+    { label: "Motocicletas", value: "Motocicletas" },
+    { label: "Barcos", value: "Barcos" },
+    { label: "Outro", value: "Outro" },
+  ]);
+
+  const currentYear = new Date().getFullYear();
+  const years = Array.from(
+    { length: currentYear - 1980 + 1 },
+    (_, i) => currentYear - i
+  );
+
+  const [yearItems, setYearItems] = useState(
+    years.map((year) => ({ label: year.toString(), value: year }))
+  );
+
+  const {
+    control,
+    handleSubmit,
+    formState: { errors },
+    reset,
+  } = useForm({
+    defaultValues: {
+      title: "",
+      type: "",
+      year: "",
+      brand: "",
+      model: "",
+      mileage: "",
+      price: "",
+      description: "",
+    },
+  });
+
+  const pickImage = async () => {
+    let result = await ImagePicker.launchImageLibraryAsync({
+      mediaTypes: [
+        ImagePicker.MediaTypeOptions.Images,
+        ImagePicker.MediaTypeOptions.Videos,
+      ],
+      allowsEditing: true,
+      quality: 1,
+      selectionLimit: 0,
+    });
+
+    if (!result.canceled) {
+      const selectedImages = result.assets.map((asset) => asset.uri);
+      setImages([...images, ...selectedImages]);
+    }
+  };
+
+  const onSubmit = async (data) => {
+    const newVehicle = {
+      id: uuidv4(),
+      ...data,
+      price: parseFloat(data.price),
+      category: "veiculos",
+      images,
+    };
+
+    try {
+      const response = await fetch("http://localhost:3000/product", {
+        method: "POST",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify(newVehicle),
+      });
+
+      if (response.ok) {
+        Alert.alert("Sucesso", "Veículo publicado!");
+        reset();
+        setImages([]);
+      } else {
+        Alert.alert("Erro", "Não foi possível publicar.");
+      }
+    } catch (error) {
+      console.error(error);
+      Alert.alert("Erro", "Verifique o servidor.");
+    }
+
+    console.log(newVehicle);
+  };
+
+  return (
+    <KeyboardAvoidingView
+      style={{ flex: 1 }}
+      behavior={Platform.OS === "ios" ? "padding" : "height"}
+    >
+      <ScrollView
+        style={styles.container}
+        keyboardShouldPersistTaps="handled"
+        contentContainerStyle={{ paddingBottom: 100 }}
+      >
+        {/* adicionar imagem */}
+        <View style={styles.imagesContainer}>
+          {images.map((uri, index) => (
+            <Image key={index} source={{ uri }} style={styles.image} />
+          ))}
+          <TouchableOpacity style={styles.addImageButton} onPress={pickImage}>
+            <Text style={styles.addPhotoText}>
+              <ImagePlus />
+            </Text>
+            <Text style={{ fontSize: 13, color: "#333", textAlign: "center" }}>
+              Adicionar foto
+            </Text>
+          </TouchableOpacity>
+        </View>
+        {/* Título */}
+        <Controller
+          control={control}
+          name="title"
+          rules={{ required: "O título é obrigatório" }}
+          render={({ field: { onChange, value } }) => (
+            <TextInput
+              placeholder="Título"
+              style={[
+                styles.input,
+                focusedField === "title" && { borderColor: "#319BE5" },
+              ]}
+              value={value}
+              onChangeText={onChange}
+              onFocus={() => setFocusedField("title")}
+              onBlur={() => setFocusedField(null)}
+            />
+          )}
+        />
+        {errors.title && (
+          <Text style={styles.errorText}>{errors.title.message}</Text>
+        )}
+        {/* Tipo */}
+        <Controller
+          control={control}
+          name="type"
+          rules={{ required: "O tipo de veículo é obrigatório" }}
+          render={({ field: { onChange, value } }) => (
+            <View>
+              <DropDownPicker
+                style={styles.pickerContainer}
+                open={typeOpen}
+                value={value} // ✅ pega valor direto do form
+                items={typeItems}
+                setOpen={setTypeOpen}
+                setValue={onChange} // ✅ atualiza o react-hook-form
+                setItems={setTypeItems}
+                placeholder="Selecione o tipo de veículo"
+                listMode="MODAL" // não abre modal, abre inline
+                modalProps={{
+                  animationType: "slide",
+                }}
+                modalAnimationType="slide" // Tipo de animação do modal
+                modalContentContainerStyle={{
+                  height: "50%",
+                  marginTop: "auto", // faz aparecer na parte inferior
+                  borderTopLeftRadius: 20,
+                  borderTopRightRadius: 20,
+                }}
+                zIndex={3000}
+                zIndexInverse={1000}
+              />
+            </View>
+          )}
+        />
+        {errors.type && (
+          <Text style={styles.errorText}>{errors.type.message}</Text>
+        )}
+        {/* Ano */}
+        <Controller
+          control={control}
+          name="year"
+          rules={{ required: "O ano é obrigatório" }}
+          render={({ field: { onChange, value } }) => (
+            <View>
+              <DropDownPicker
+                style={styles.pickerContainer}
+                open={yearOpen}
+                value={value}
+                items={yearItems}
+                setOpen={setYearOpen}
+                setValue={onChange}
+                setItems={setYearItems}
+                placeholder="Selecione o ano"
+                listMode="MODAL" // não abre modal, abre inline
+                modalProps={{
+                  animationType: "slide",
+                }}
+                modalAnimationType="slide" // Tipo de animação do modal
+                zIndex={2000}
+                zIndexInverse={2000}
+              />
+            </View>
+          )}
+        />
+        {errors.year && (
+          <Text style={styles.errorText}>{errors.year.message}</Text>
+        )}
+        {/* Fabricante */}
+        <Controller
+          control={control}
+          name="brand"
+          rules={{ required: "O fabricante é obrigatório" }}
+          render={({ field: { onChange, value } }) => (
+            <TextInput
+              placeholder="Fabricante"
+              style={[
+                styles.input,
+                focusedField === "brand" && { borderColor: "#319BE5" },
+              ]}
+              value={value}
+              onChangeText={onChange}
+              onFocus={() => setFocusedField("brand")}
+              onBlur={() => setFocusedField(null)}
+            />
+          )}
+        />
+        {errors.title && (
+          <Text style={styles.errorText}>{errors.brand.message}</Text>
+        )}
+        {/* Modelo */}
+        <Controller
+          control={control}
+          name="model"
+          rules={{ required: "O modelo é obrigatório" }}
+          render={({ field: { onChange, value } }) => (
+            <TextInput
+              placeholder="Modelo"
+              style={[
+                styles.input,
+                focusedField === "model" && { borderColor: "#319BE5" },
+              ]}
+              value={value}
+              onChangeText={onChange}
+              onFocus={() => setFocusedField("model")}
+              onBlur={() => setFocusedField(null)}
+            />
+          )}
+        />
+        {errors.model && (
+          <Text style={styles.errorText}>{errors.model.message}</Text>
+        )}
+        {/* Quilometragem */}
+        <Controller
+          control={control}
+          name="mileage"
+          rules={{
+            required: "A quilometragem é obrigatória",
+            pattern: {
+              value: /^\d+$/,
+              message: "Digite uma quilometragem válida",
+            },
+          }}
+          render={({ field: { onChange, value } }) => (
+            <TextInput
+              placeholder="Quilometragem"
+              style={[
+                styles.input,
+                focusedField === "mileage" && { borderColor: "#319BE5" },
+              ]}
+              value={value}
+              onChangeText={onChange}
+              onFocus={() => setFocusedField("mileage")}
+              onBlur={() => setFocusedField(null)}
+              keyboardType="numeric"
+            />
+          )}
+        />
+        {errors.mileage && (
+          <Text style={styles.errorText}>{errors.mileage.message}</Text>
+        )}
+        {/* Preço */}
+        <Controller
+          control={control}
+          name="price"
+          rules={{
+            required: "O preço é obrigatório",
+            pattern: {
+              value: /^\d+(\.\d{1,2})?$/,
+              message: "Digite um preço válido",
+            },
+          }}
+          render={({ field: { onChange, value } }) => (
+            <TextInput
+              placeholder="Preço"
+              style={[
+                styles.input,
+                focusedField === "price" && { borderColor: "#319BE5" },
+              ]}
+              value={value}
+              onChangeText={onChange}
+              onFocus={() => setFocusedField("price")}
+              onBlur={() => setFocusedField(null)}
+              keyboardType="numeric"
+            />
+          )}
+        />
+        {errors.price && (
+          <Text style={styles.errorText}>{errors.price.message}</Text>
+        )}
+        {/* Descrição */}
+        <Controller
+          control={control}
+          name="description"
+          render={({ field: { onChange, value } }) => (
+            <TextInput
+              placeholder="Descrição (opcional)"
+              style={[
+                styles.input,
+                { height: 100, textAlignVertical: "top" },
+                focusedField === "description" && { borderColor: "#319BE5" },
+              ]}
+              value={value}
+              onChangeText={onChange}
+              onFocus={() => setFocusedField("description")}
+              onBlur={() => setFocusedField(null)}
+              multiline
+            />
+          )}
+        />
+        {/* Botão Publicar */}
+        <View style={{ marginBottom: 30 }}>
+          <TouchableOpacity
+            style={styles.publishButton}
+            onPress={handleSubmit(onSubmit)}
+          >
+            <Text style={styles.publishButtonText}>Publicar</Text>
+          </TouchableOpacity>
+        </View>
+      </ScrollView>
+    </KeyboardAvoidingView>
+  );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    padding: 20,
+    backgroundColor: "#fff",
+  },
+  imagesContainer: {
+    flexDirection: "row",
+    alignItems: "center",
+    justifyContent: "center",
+    marginBottom: 20,
+    flexWrap: "wrap",
+  },
+  image: {
+    width: 80,
+    height: 80,
+    borderRadius: 10,
+    marginRight: 10,
+    marginBottom: 10,
+  },
+  addImageButton: {
+    width: 80,
+    height: 80,
+    borderRadius: 10,
+    backgroundColor: "#f0f0f0",
+    justifyContent: "center",
+    alignItems: "center",
+    marginBottom: 10,
+  },
+  addPhotoText: {
+    fontSize: 30,
+    color: "#333",
+  },
+  input: {
+    borderWidth: 1,
+    borderColor: "#ccc",
+    borderRadius: 8,
+    padding: 10,
+    marginBottom: 15,
+  },
+  pickerContainer: {
+    borderWidth: 1,
+    borderColor: "#ccc",
+    borderRadius: 8,
+    marginBottom: 15,
+    height: 50,
+    justifyContent: "center",
+    backgroundColor: "#fff",
+    overflow: "hidden",
+  },
+  publishButton: {
+    backgroundColor: "#319BE5",
+    padding: 15,
+    borderRadius: 8,
+    lignItems: "center",
+    marginTop: 10,
+  },
+  publishButtonText: {
+    color: "#fff",
+    fontSize: 16,
+    fontWeight: "bold",
+  },
+  errorText: {
+    color: "red",
+    marginBottom: 10,
+  },
+});
