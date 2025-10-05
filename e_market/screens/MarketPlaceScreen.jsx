47c413c2a86962552a1aaf07f5a9fae2d18791cb
Wesley Marques <marques_bertin@hotmail.com>
2025-10-05 18:03:35 -0300

    feat: search bar

--- e_market/screens/MarketPlaceScreen.jsx
+++ e_market/screens/MarketPlaceScreen.jsx
@@ -1,8 +1,13 @@
-import { View, Text, StyleSheet, FlatList } from "react-native";
+import { View, Text, StyleSheet, FlatList, TextInput } from "react-native";
 import { useEffect, useState } from "react";
 import ProductCard from "../components/Card";
 import { useDispatch, useSelector } from "react-redux";
-import { fetchProducts, selectAllProducts, selectError, selectLoading } from "../store/features/productSlice";
+import {
+  fetchProducts,
+  selectAllProducts,
+  selectError,
+  selectLoading,
+} from "../store/features/productSlice";
 
 export default function MarketplaceScreen() {
   const dispatch = useDispatch();
@@ -10,16 +15,41 @@ export default function MarketplaceScreen() {
   const error = useSelector(selectError);
   const loading = useSelector(selectLoading);
 
+  const [focusedField, setFocusedField] = useState(null); 
+
+  const [searchQuery, setSearchQuery] = useState("");
+  const [filteredProducts, setFilteredProducts] = useState([]);
+
   useEffect(() => {
     dispatch(fetchProducts());
   }, [dispatch]);
 
+  useEffect(() => {
+    if (searchQuery === "") {
+      setFilteredProducts(products); // sem filtro, mostra todos
+    } else {
+      const filtered = products.filter((p) =>
+        p.title.toLowerCase().includes(searchQuery.toLowerCase())
+      );
+      setFilteredProducts(filtered);
+    }
+  }, [searchQuery, products]);
+
   return (
     <View style={styles.container}>
-      
-      {products && products.length > 0 ? (
+      {/* Barra de pesquisa */}
+      <TextInput
+        placeholder="Buscar produtos..."
+        style={styles.searchInput}
+        value={searchQuery}
+        onChangeText={setSearchQuery}
+      />
+
+      {loading ? (
+        <Text>Carregando produtos...</Text>
+      ) : filteredProducts && filteredProducts.length > 0 ? (
         <FlatList
-          data={products}
+          data={filteredProducts}
           keyExtractor={(item) => item.id.toString()}
           renderItem={({ item }) => (
             <ProductCard
@@ -29,10 +59,10 @@ export default function MarketplaceScreen() {
             />
           )}
           numColumns={2}
-          columnWrapperStyle={{ justifyContent: 'flex-start', marginBottom: 2, gap: 2 }}
+          columnWrapperStyle={{ justifyContent: "flex-start", marginBottom: 2, gap: 2 }}
         />
       ) : (
-        <Text>Carregando produtos...</Text>
+        <Text>Nenhum produto encontrado.</Text>
       )}
     </View>
   );
@@ -43,5 +73,13 @@ const styles = StyleSheet.create({
     flex: 1,
     paddingTop: 16,
     paddingHorizontal: 12,
+  },
+  searchInput: {
+    height: 40,
+    borderWidth: 1,
+    borderColor: "#ccc",
+    borderRadius: 8,
+    paddingHorizontal: 12,
+    marginBottom: 12,
   },
 });
