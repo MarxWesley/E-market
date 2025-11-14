34d5379481428eec638edeb4d4b675f2befbd96b
Wesley Marques <Marques_bertin@hotmail.com>
2025-11-13 21:28:40 -0300

    dsad

--- e_market/services/favoriteService.js
+++ e_market/services/favoriteService.js
@@ -2,7 +2,7 @@ import api from "./api";
 
 const getFavoriteByUserId = async (userId) => {
   try {
-    const response = await api.get(`/favorites?userId=${userId}`);
+    const response = await api.get(`/favorite?userId=${userId}`);
     return response.data;
   } catch (error) {
     console.error("Error fetching favorite items:", error);
@@ -11,12 +11,12 @@ const getFavoriteByUserId = async (userId) => {
 };
 
 const addFavorite = async (favorite) => {
-  const response = await api.post(`/favorites`, favorite);
+  const response = await api.post(`/favorite`, favorite);
   return response.data;
 };
 
 const removeFavorite = async (id) => {
-  const response = await api.delete(`/favorites/${id}`);
+  const response = await api.delete(`/favorite/${id}`);
   return response.data;
 };
 
